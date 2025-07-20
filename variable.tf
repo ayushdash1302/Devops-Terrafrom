5395fa514801b3d5a5c672041cef0377f17caa61
Ayush Dash <120770632+ayushdash1302@users.noreply.github.com>
2025-07-20 14:27:14 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,29 @@
++++++++++++++++++
+Variable
++++++++++++++++++
+
+---------------------------------------------------------
+Variable.tf
+---------------------------------------------------------
+
+# variable.tf 
+
+# No default value
+variable "instance_type" {
+   type = string
+   description = "EC2 Instance Type"
+}
+
+# No default value
+variable "tag" {
+   type = string
+   description = "The tag for the EC2 instance"
+}
+
+# default value for the variable location
+variable "location" {
+   type = string
+   description = "The project region"
+   default = "us-east-1"
+}
 
